982eccf02bdeda73d098a181149ce555a3787585
itning <itning@itning.top>
2019-04-15 14:41:14 +0800

    add allow cors

--- src/main/java/com/sl/shiro/AuthenticationFilter.java
+++ src/main/java/com/sl/shiro/AuthenticationFilter.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpMethod;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.http.HttpHeaders.*;
 
 /**
  * @author itning
@@ -15,11 +18,22 @@ public class AuthenticationFilter extends FormAuthenticationFilter {
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
         if (request instanceof HttpServletRequest) {
-            System.out.println(HttpMethod.OPTIONS.name());
+            HttpServletResponse resp = (HttpServletResponse) response;
+            HttpServletRequest req = (HttpServletRequest) request;
+            allowCors(resp, req);
             if (((HttpServletRequest) request).getMethod().toUpperCase().equals(HttpMethod.OPTIONS.name())) {
                 return true;
             }
         }
         return super.isAccessAllowed(request, response, mappedValue);
+    }
+
+    private void allowCors(HttpServletResponse resp, HttpServletRequest req) {
+        String origin = req.getHeader(ORIGIN);
+        resp.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        resp.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+        resp.setHeader(ACCESS_CONTROL_ALLOW_METHODS, "POST,GET,OPTIONS,DELETE,PUT,PATCH");
+        resp.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, req.getHeader(ACCESS_CONTROL_REQUEST_HEADERS));
+        resp.setIntHeader(ACCESS_CONTROL_MAX_AGE, 2592000);
     }
 }
